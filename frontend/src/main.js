664cc6aa09f60d4aec7a0092447ff63e0476c2f4
Hobai Riku <me@hobairiku.site>
2018-07-23 15:52:20 +0800

    update ignore
    update ignore

---
+++ frontend/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/style.css'
+import ElementUI from 'element-ui'
+import { timeFilter } from './util/timeFilter'
+Vue.use(router)
+Vue.filter('timeFilter', timeFilter)
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
